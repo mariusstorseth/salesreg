a53ede2d13c994d9c56f346265e5a80db321504f
Marius Størseth <marius.storseth@gmail.com>
2014-06-13 17:11:49 +0200

    Added seller list with budget

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -1,2 +1,12 @@
 module BudgetsHelper
+
+  def seller_budget(seller, month)
+    if seller.budgets.search(month).sum(:amount) != 0
+      return seller.budgets.search(month).sum(:amount)
+    else
+      return link_to 'Add', new_budget_path
+    end
+  end
 end
+
+
